64b21fbc6e4ae195ceadb6eabd35382f20d220f8
Soojeong Choi <78012039+Soodev20@users.noreply.github.com>
2022-05-01 21:11:20 +0900

    Update 043022_FindLeaves.js
    Clean: shortcut trial

--- 043022_FindLeaves.js
+++ 043022_FindLeaves.js
@@ -30,34 +30,3 @@
 
   return leaves1.length === leaves2.length && leaves1.every((value, i) => value === leaves2[i]);
 };
-
-// tried short cut, perhaps need to use yield?
-// let leafSimilar = function (root1, root2) {
-//   let leaves1 = [];
-//   let leaves2 = [];
-
-//   const findLeaves = function (node1, node2, leaves1) {
-//     if (node1) {
-//       if (!node1.left && !node1.right) leaves1.push(node.val);
-
-//       findLeaves(node1.left, node2, leaves1);
-//       findLeaves(node1.right, node2, leaves1);
-//     }
-
-//     if (node2) {
-//       if (!node2.left && !node2.right) {
-//         if (node2.val !== leaves[0]) {
-//           return false;
-//         }
-//         leaves1.shift();
-//       }
-
-//       findLeaves(node1, node2.left, leaves1);
-//       findLeaves(node1, node2.right, leaves1);
-//     }
-
-//     return true;
-//   }
-// };
-
-
